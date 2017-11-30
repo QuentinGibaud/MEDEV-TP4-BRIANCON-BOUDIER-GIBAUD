b50eacbfb78ad0d5d12b1bb65ffdf3c7a6406e5f
LocaBoubou <justine.boudier72@gmail.com>
2017-11-30 12:44:57 +0100

    test initialisePlateau

--- test/jeudedames/PlateauTest.java
+++ test/jeudedames/PlateauTest.java
@@ -42,22 +42,22 @@ public class PlateauTest {
         instance.initialisePlateau();
         ArrayList<Pion> pionNoir = instance.getJoueurNoir().getPions();
         ArrayList<Pion> pionBlanc = instance.getJoueurBlanc().getPions();
-        Point2D point1 = new Point2D(0,3);
-        Point2D point2 = new Point2D(1,6);
-        Point2D point3 = new Point2D(2,1);
-        Point2D point4 = new Point2D(3,8);
-        Point2D point5 = new Point2D(6,3);
-        Point2D point6 = new Point2D(7,6);
-        Point2D point7 = new Point2D(8,1);
-        Point2D point8 = new Point2D(9,8);
-        assertTrue(pionNoir.contains(point1));
-        assertTrue(pionNoir.contains(point2));
-        assertTrue(pionNoir.contains(point3));
-        assertTrue(pionNoir.contains(point4));
-        assertTrue(pionBlanc.contains(point5));
-        assertTrue(pionBlanc.contains(point6));
-        assertTrue(pionBlanc.contains(point7));
-        assertTrue(pionBlanc.contains(point8));
+        Simple pion1 =new Simple(new Point2D(0,3));
+        Simple pion2 = new Simple(new Point2D(1,6));
+        Simple pion3 = new Simple(new Point2D(2,1));
+        Simple pion4 = new Simple(new Point2D(3,8));
+        Simple pion5 = new Simple(new Point2D(6,3));
+        Simple pion6 = new Simple(new Point2D(7,6));
+        Simple pion7 = new Simple(new Point2D(8,1));
+        Simple pion8 = new Simple(new Point2D(9,8));
+        assertTrue(pionNoir.contains(pion1));
+        assertTrue(pionNoir.contains(pion2));
+        assertTrue(pionNoir.contains(pion3));
+        assertTrue(pionNoir.contains(pion4));
+        assertTrue(pionBlanc.contains(pion5));
+        assertTrue(pionBlanc.contains(pion6));
+        assertTrue(pionBlanc.contains(pion7));
+        assertTrue(pionBlanc.contains(pion8));
     }
     
 }
