31bd22c2afd83948107e19153ec8ce845621bfe2
QuentinGibaud <quentgib@gmail.com>
2017-11-30 08:56:50 +0100

    Ajout du package de test

---
+++ test/jeudedames/JoueurTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeudedames;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Quentin GIBAUD
+ */
+public class JoueurTest {
+    
+    public JoueurTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getNom method, of class Joueur.
+     */
+    @Test
+    public void testGetNom() {
+        System.out.println("getNom");
+        Joueur instance = new Joueur();
+        String expResult = "";
+        String result = instance.getNom();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNom method, of class Joueur.
+     */
+    @Test
+    public void testSetNom() {
+        System.out.println("setNom");
+        String nom = "";
+        Joueur instance = new Joueur();
+        instance.setNom(nom);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
